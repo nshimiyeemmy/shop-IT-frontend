687db1057857b3be10b54e3c29dec5ca3efc66be
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-16 09:32:03 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
+import Home from './components/Home'
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Home/>
      <Footer/>
     </div>
   )

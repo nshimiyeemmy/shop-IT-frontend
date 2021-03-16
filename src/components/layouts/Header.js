b606dc8895c8a1a7ba15b48c71df0ae9acad6b7a
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-16 07:11:52 +0200

    updated

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -3,10 +3,10 @@ import '../../App.css'
 const Header = () => {
     return (
         <Fragment>
-            <nav className="navbar row">
+      <nav className="navbar row">
       <div className="col-12 col-md-3">
-        <div className="navbar-brand">
-          <img src="./images/logo.png" />
+      <div className="navbar-brand">
+       <img src="./images/logo.png" />
         </div>
       </div>
 

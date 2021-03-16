7bc59f42240e3a469d6975c095c7e7e412e226b9
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-16 11:48:18 +0200

    updated with react route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
 import Home from './components/Home'
 function App() {
   return (
+    <Router>
     <div className="App">
      <Header/>
-     <Home/>
+     <div className="container container-fluid home">
+     <Route path="/" component={Home} exact/>
+     </div>
      <Footer/>
     </div>
+    </Router>
   )
 }
 

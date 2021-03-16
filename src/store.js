5d36dafd1b7bb0bdf2b2b3ca327177cff56dd0be
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-16 16:19:05 +0200

    added functionality to add redux in our app for state management

---
+++ src/store.js
@@ -0,0 +1,22 @@
+//contains the codes to connect to the extension {react-redux}
+
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+//creating reducers for every resource such as users, products
+const reducer = combineReducers({
+
+})
+
+//this inital state contains all the data that we want to put in the state just before loading the application
+let initialState = {}
+
+//contains all the middleware that we want to use
+const middleware = [
+    thunk
+]
+
+const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store

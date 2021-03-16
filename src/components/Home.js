1e6bb4e9fd0b350adaac13c7e4a616363c7984cd
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-03-16 21:35:08 +0200

    updated with gettinng our products from  state and display them on our page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,17 @@ import {getProducts} from '../actions/productActions'
 const Home = () => {
  
   const dispatch = useDispatch();
+  //fetching data from the state
+ const {products,loading, error, productsCount} = useSelector(state =>state.products)
+
+console.log(products,productsCount)
+
   //useEffect is the hook that is going to run this component basically routes, it is the first thing to run when this component loads
   //It's much like a constructor of the class
   useEffect(() => {
 //so inside here we have to call our function
    dispatch(getProducts());
+   
   },[dispatch])
 
 
@@ -21,40 +27,19 @@ const Home = () => {
 
     <section id="products" className="container mt-5">
       <div className="row">
-        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div className="card p-3 rounded">
-            <img
-              className="card-img-top mx-auto"
-              src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">
-                <a href="">128GB Solid Storage Memory card - SanDisk Ultra</a>
-              </h5>
-              <div className="ratings mt-auto">
-                <div className="rating-outer">
-                  <div className="rating-inner"></div>
-                </div>
-                <span id="no_of_reviews">(5 Reviews)</span>
-              </div>
-              <p className="card-text">$45.67</p>
-              <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-            </div>
-          </div>
-        </div>
-  
-        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
+        {products && products.map(product => (
+
+          <div key={product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
           <div className="card p-3 rounded">
             <img
               className="card-img-top mx-auto"
               src="https://m.media-amazon.com/images/I/61B04f0ALWL._AC_UY218_.jpg"
             />
             <div className="card-body d-flex flex-column">
-              <h5 className="card-title">
-                <a href=""
-                  >Wyze Cam 1080p HD Indoor Wireless Smart Home Camera Wyze Cam 1080p HD Indoor Wireless Smart Home Camera</a
-                >
+             <h5 className="card-title">
+                <a href="">{product.name}</a>
               </h5>
+              </div>
               <div className="ratings mt-auto">
                <i className="fa fa-star"></i>
                 <i className="fa fa-star"></i>
@@ -67,58 +52,8 @@ const Home = () => {
               <a href="#" id="view_btn" className="btn btn-block">View Details</a>
             </div>
           </div>
-        </div>
 
-        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div className="card p-3 rounded">
-            <img
-              className="card-img-top mx-auto"
-              src="https://m.media-amazon.com/images/I/813oF-FY01L._AC_UY218_.jpg"
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">
-                <a href=""
-                  >Fujifilm Instax Mini Instant Film Twin Pack (White)</a
-                >
-              </h5>
-              <div className="ratings mt-auto">
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star-half-o"></i>
-                <i className="fa fa-star-o"></i>
-                <span id="no_of_reviews">(5 Reviews)</span>
-              </div>
-              <p className="card-text">$125.57</p>
-              <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div className="card p-3 rounded">
-            <img
-              className="card-img-top mx-auto"
-              src="https://m.media-amazon.com/images/I/61pBvlYVPxL._AC_UY218_.jpg"
-            />
-            <div className="card-body d-flex flex-column">
-              <h5 className="card-title">
-                <a href="">AmazonBasics High-Speed HDMI Cable</a>
-              </h5>
-              <div className="ratings mt-auto">
-               <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star"></i>
-                <i className="fa fa-star-half-o"></i>
-                <i className="fa fa-star-o"></i>
-                <span id="no_of_reviews">(5 Reviews)</span>
-              </div>
-              <p className="card-text">$75.56</p>
-
-              <a type="button" href="#" id="view_btn" className="btn btn-block">View Details</a>
-            </div>
-          </div>
-        </div>
+        ))}
        </div>
     </section>
             

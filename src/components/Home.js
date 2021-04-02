6b79bbd4edc34d305fe515fa9e713404109608bb
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-02 09:06:51 +0200

    added loader for loading all the data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from 'react'
 import MetaData from './layouts/MetaData'
 import Product from './product/Product'
+import Loader from './layouts/Loader'
 import { useDispatch, useSelector } from 'react-redux'
 import {getProducts} from '../actions/productActions'
 const Home = () => {
@@ -21,20 +22,24 @@ console.log(products,productsCount)
 
 
     return (
-        <Fragment >
-          <MetaData title={'Buy best products online'}/>
-
-        <h1 id="products_heading">Latest Products</h1>
-
-    <section id="products" className="container mt-5">
-      <div className="row">
-        {products && products.map(product => (
-        <Product key={product._id} product={product}/>
-        ))}
-       </div>
-    </section>
-            
-        </Fragment>
+      <Fragment>
+        {loading ? <Loader/>:(
+           <Fragment >
+           <MetaData title={'Buy best products online'}/>
+ 
+         <h1 id="products_heading">Latest Products</h1>
+ 
+     <section id="products" className="container mt-5">
+       <div className="row">
+         {products && products.map(product => (
+         <Product key={product._id} product={product}/>
+         ))}
+        </div>
+     </section>
+             
+         </Fragment>
+        )}
+      </Fragment>  
     )
 }
 export default Home

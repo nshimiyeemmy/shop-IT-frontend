3df77f20394f07f8e4fd05b9851be9fc11461051
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-02 08:49:23 +0200

    added the products page into our how

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from 'react'
 import MetaData from './layouts/MetaData'
+import Product from './product/Product'
 import { useDispatch, useSelector } from 'react-redux'
 import {getProducts} from '../actions/productActions'
 const Home = () => {
@@ -28,29 +29,7 @@ console.log(products,productsCount)
     <section id="products" className="container mt-5">
       <div className="row">
         {products && products.map(product => (
-
-          <div key={product._id} className="col-sm-12 col-md-6 col-lg-3 my-3">
-          <div className="card p-3 rounded">
-            <img
-              className="card-img-top mx-auto"
-              src={product.images[0].url}
-            />
-            <div className="card-body d-flex flex-column">
-             <h5 className="card-title">
-                <a href="">{product.name}</a>
-              </h5>
-              </div>
-              <div className="ratings mt-auto">
-                <div className="rating-outer">
-                  <div className="rating-inner" style={{width:`${(product.ratings / 5) * 100}%`}}>
-                  </div>
-                </div>
-                <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
-              </div>
-              <p className="card-text">${product.price}</p>
-              <a href="#" id="view_btn" className="btn btn-block">View Details</a>
-            </div>
-          </div>
+        <Product key={product._id} product={product}/>
         ))}
        </div>
     </section>

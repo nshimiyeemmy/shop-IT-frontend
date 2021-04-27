041d0d001960e5188ff41f4236a8cb06827ae974
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-27 16:36:08 +0200

    added alert functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,32 +3,31 @@ import MetaData from './layouts/MetaData'
 import Product from './product/Product'
 import Loader from './layouts/Loader'
 import { useDispatch, useSelector } from 'react-redux'
+import {useAlert} from 'react-alert'
 import {getProducts} from '../actions/productActions'
 const Home = () => {
- 
+
+  const alert = useAlert();
   const dispatch = useDispatch();
   //fetching data from the state
  const {products,loading, error, productsCount} = useSelector(state =>state.products)
-
-console.log(products,productsCount)
-
   //useEffect is the hook that is going to run this component basically routes, it is the first thing to run when this component loads
   //It's much like a constructor of the class
   useEffect(() => {
 //so inside here we have to call our function
-   dispatch(getProducts());
-   
-  },[dispatch])
-
-
+if(error){
+    return alert.error(error)
+    }
+    dispatch(getProducts());
+  },[dispatch, alert, error])
     return (
       <Fragment>
         {loading ? <Loader/>:(
            <Fragment >
            <MetaData title={'Buy best products online'}/>
- 
+
          <h1 id="products_heading">Latest Products</h1>
- 
+
      <section id="products" className="container mt-5">
        <div className="row">
          {products && products.map(product => (
@@ -36,10 +35,10 @@ console.log(products,productsCount)
          ))}
         </div>
      </section>
-             
+
          </Fragment>
         )}
-      </Fragment>  
+      </Fragment>
     )
 }
 export default Home

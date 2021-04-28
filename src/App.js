94675325f06fbbf49228ad7730329fdeb51f3aa1
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-28 20:00:13 +0200

    updated with get product by ID

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
 import Home from './components/Home'
+import productDetails from './components/product/ProductDetails'
 function App() {
   return (
     <Router>
@@ -9,6 +10,7 @@ function App() {
      <Header/>
      <div className="container container-fluid home">
      <Route path="/" component={Home} exact/>
+     <Route path="/product/:id" component={productDetails} exact/>
      </div>
      <Footer/>
     </div>

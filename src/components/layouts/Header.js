f6ef57037f170297592b55f71b599612d75afb41
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-04-29 21:51:54 +0200

    improved the way we implement searchfunctionality

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,4 +1,6 @@
 import React, { Fragment } from 'react'
+import {Route} from 'react-router-dom'
+import Search from '../layouts/Search'
 import '../../App.css'
 const Header = () => {
     return (
@@ -11,19 +13,8 @@ const Header = () => {
       </div>
 
       <div className="col-12 col-md-6 mt-2 mt-md-0">
-        <div className="input-group">
-          <input
-            type="text"
-            id="search_field"
-            className="form-control"
-            placeholder="Enter Product Name ..."
-          />
-          <div className="input-group-append">
-            <button id="search_btn" className="btn">
-              <i className="fa fa-search" aria-hidden="true"></i>
-            </button>
-          </div>
-        </div>
+        {/* this is a placeholder for the search component */}
+        <Route render={({history})=> <Search history={history} />} />
       </div>
 
       <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">

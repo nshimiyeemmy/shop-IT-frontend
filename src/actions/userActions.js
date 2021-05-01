50f5073a56d95d19880d840f6e5e44a3ea43f63d
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-01 08:21:48 +0200

    added userActions

---
+++ src/actions/userActions.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import {
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    CLEAR_ERRORS
+
+} from '../constants/userConstants'
+
+//Login action
+export const login = (email,password) => async dispatch =>{
+
+    try {
+
+        dispatch({type:LOGIN_REQUEST});
+        const config = {
+            headers:{
+               'Content-Type':'application/json',
+            }
+        }
+        const data = await axios.post(`/api/v1/login`, { email,password },config)
+        dispatch({
+            type:LOGIN_SUCCESS,
+            payload:{user:data.data}
+
+        })
+    } catch (error) {
+        dispatch({
+            type:LOGIN_FAIL,
+            payload:error.response.data.message
+        })
+    }
+
+}

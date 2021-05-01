21655b13a3402f9355f4469586f60f8a8c69a49a
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-01 13:40:15 +0200

    updated with user register constants

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -3,6 +3,9 @@ import {
     LOGIN_REQUEST,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_FAIL,
     CLEAR_ERRORS
 
 } from '../constants/userConstants'
@@ -31,6 +34,34 @@ export const login = (email,password) => async dispatch =>{
         })
     }
 }
+
+//register user
+export const register = (userData) => async dispatch =>{
+
+    try {
+
+        dispatch({type:REGISTER_USER_REQUEST});
+        const config = {
+            headers:{
+               'Content-Type':'multipart/form-data',
+            }
+        }
+        const data = await axios.post(`/api/v1/register`, { userData },config)
+        dispatch({
+            type:REGISTER_USER_SUCCESS,
+            payload:{user:data.data}
+
+        })
+    } catch (error) {
+        dispatch({
+            type:REGISTER_USER_FAIL,
+            payload:error.response.data.message
+        })
+    }
+}
+
+
+
     //Clear Erorrs using CLEAR_ERRORS constant
     export const clearErrors = () =>async(dispatch)=>{
         dispatch({

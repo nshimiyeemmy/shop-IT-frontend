867d5797a0d844aed23fe72d91fffdb958a4aadf
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:20:55 +0200

    updated

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,32 +1,40 @@
-import React, { Fragment } from 'react'
-import {Route,Link} from 'react-router-dom'
-import Search from '../layouts/Search'
-import '../../App.css'
+import React, { Fragment } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Search from '../layouts/Search';
+import '../../App.css';
 const Header = () => {
     return (
         <Fragment>
-      <nav className="navbar row">
-      <div className="col-12 col-md-3">
-      <div className="navbar-brand">
-          <Link to="/">
-       <img src="./images/logo.png" />
-          </Link>
-        </div>
-      </div>
+            <nav className="navbar row">
+                <div className="col-12 col-md-3">
+                    <div className="navbar-brand">
+                        <Link to="/">
+                            <img src="./images/logo.png" />
+                        </Link>
+                    </div>
+                </div>
 
-      <div className="col-12 col-md-6 mt-2 mt-md-0">
-        {/* this is a placeholder for the search component */}
-        <Route render={({history})=> <Search history={history} />} />
-      </div>
+                <div className="col-12 col-md-6 mt-2 mt-md-0">
+                    {/* this is a placeholder for the search component */}
+                    <Route
+                        render={({ history }) => <Search history={history} />}
+                    />
+                </div>
 
-      <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-        <Link to="/login" className="btn ml-4" id="login_btn">Login</Link>
-        <span id="cart" className="ml-3">Cart</span>
-        <span className="ml-1" id="cart_count">2</span>
-      </div>
-    </nav>
+                <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+                    <Link to="/login" className="btn ml-4" id="login_btn">
+                        Login
+                    </Link>
+                    <span id="cart" className="ml-3">
+                        Cart
+                    </span>
+                    <span className="ml-1" id="cart_count">
+                        2
+                    </span>
+                </div>
+            </nav>
         </Fragment>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;

1c6a8dc5a39ffd51957be5582421935a615b0dfa
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 01:47:09 +0200

    added the user reducer to the store

--- src/store.js
+++ src/store.js
@@ -1,21 +1,29 @@
 //contains the codes to connect to the extension {react-redux}
 
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import {productsReducers,productDetailsReducer} from './reducers/productReducers'
-import {authReducer} from './reducers/userReducers'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  productsReducers,
+  productDetailsReducer,
+} from './reducers/productReducers';
+import { authReducer, userReducer } from './reducers/userReducers';
 //creating reducers for every resource such as users, products
 const reducer = combineReducers({
- products:productsReducers,
- productDetails:productDetailsReducer,
- auth:authReducer
-})
+  products: productsReducers,
+  productDetails: productDetailsReducer,
+  auth: authReducer,
+  user: userReducer,
+});
 //this inital state contains all the data that we want to put in the state just before loading the application
-let initialState = {}
+let initialState = {};
 
 //contains all the middleware that we want to use
-const middleware = [thunk]
-const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
+const middleware = [thunk];
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store
+export default store;

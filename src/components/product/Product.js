da846ee41546e0f7e6b3f6c860a82dd043f72373
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:22:03 +0200

    updated

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-
 const Product = ({product,col}) => {
     return (
-
         <div className={`col-sm-12 col-md-6 col-lg-${col} my-3`}>
         <div className="card p-3 rounded">
           <img

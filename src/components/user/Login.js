e5160567319c7d2882065e809a9641a8556a885e
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:22:50 +0200

    updated

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,90 +1,91 @@
-import React, {Fragment,useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import {useAlert} from 'react-alert'
-import Loader from '../layouts/Loader'
-import MetaData from '../layouts/MetaData'
-import {login, clearErrors} from '../../actions/userActions'
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useAlert } from 'react-alert';
+import Loader from '../layouts/Loader';
+import MetaData from '../layouts/MetaData';
+import { login, clearErrors } from '../../actions/userActions';
 
-const Login = ({history}) => {
+const Login = ({ history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { loading, isAuthenticated, error } = useSelector(
+    (state) => state.auth
+  );
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (isAuthenticated) {
+      alert.success('Logged in successfully');
+      dispatch(clearErrors());
+      history.push(`/`);
+    }
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+  }, [dispatch, alert, isAuthenticated, error, history]);
 
-    const [email,setEmail] = useState('');
-    const [password,setPassword] = useState('');
-    const {loading,isAuthenticated,user,error} = useSelector(state => state.auth);
+  //creating the submitHandler to handle the email and password submit
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
 
-
-   const alert = useAlert();
-   const dispatch = useDispatch();
-
-    useEffect(() => {
-
-        if(isAuthenticated){
-            history.push(`/`)
-        }
-        if(error){
-            alert.error(error);
-            dispatch(clearErrors())
-            }
-          },[dispatch, alert,isAuthenticated,error,history])
-
-   //creating the submitHandler to handle the email and password submit
-   const submitHandler = (e) =>{
-       e.preventDefault();
-       dispatch(login(email,password))
-   }
-
-    return (
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
         <Fragment>
-            {loading ? <Loader/> : (
-            <Fragment>
-                <MetaData title={'Login'} />
-                {/* add the html for login below */}
-                <div className="row wrapper">
-		<div className="col-10 col-lg-5">
-        <form className="shadow-lg" onSubmit={submitHandler}>
-            <h1 className="mb-3">Login</h1>
-            <div className="form-group">
-              <label htmlFor="email_field">Email</label>
-              <input
-                type="email"
-                id="email_field"
-                className="form-control"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value)}
-              />
+          <MetaData title={'Login'} />
+          {/* add the html for login below */}
+          <div className="row wrapper">
+            <div className="col-10 col-lg-5">
+              <form className="shadow-lg" onSubmit={submitHandler}>
+                <h1 className="mb-3">Login</h1>
+                <div className="form-group">
+                  <label htmlFor="email_field">Email</label>
+                  <input
+                    type="email"
+                    id="email_field"
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password_field">Password</label>
+                  <input
+                    type="password"
+                    id="password_field"
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <Link to="/password/forgot" className="float-right mb-4">
+                  Forgot Password?
+                </Link>
+                <button
+                  id="login_button"
+                  type="submit"
+                  className="btn btn-block py-3"
+                >
+                  LOGIN
+                </button>
+
+                <Link to="/register" className="float-right mt-3">
+                  New User?
+                </Link>
+              </form>
             </div>
-
-            <div className="form-group">
-              <label htmlFor="password_field">Password</label>
-              <input
-                type="password"
-                id="password_field"
-                className="form-control"
-                value={password}
-                onChange={(e)=>setPassword(e.target.value)}
-
-              />
-            </div>
-
-            <Link to="/password/forgot" className="float-right mb-4">Forgot Password?</Link>
-
-            <button
-              id="login_button"
-              type="submit"
-              className="btn btn-block py-3"
-            >
-              LOGIN
-            </button>
-
-            <Link to="/register" className="float-right mt-3">New User?</Link>
-          </form>
-		  </div>
-    </div>
-
-            </Fragment>
-            )}
+          </div>
         </Fragment>
-    )
-}
+      )}
+    </Fragment>
+  );
+};
 
-export default Login
+export default Login;

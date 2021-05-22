4a89914d76e011ab270de80d3ea365c5686dc208
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 16:19:24 +0200

    updated with regisrer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Header from './components/layouts/Header'
-import Footer from './components/layouts/Footer'
-import Home from './components/Home'
-import productDetails from './components/product/ProductDetails'
-import Login from './components/user/Login'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import Home from './components/Home';
+import productDetails from './components/product/ProductDetails';
+import Login from './components/user/Login';
+import Register from './components/user/Register';
+
 function App() {
   return (
     <Router>
-    <div className="App">
-     <Header/>
-     <div className="container container-fluid home">
-     <Route path="/" component={Home} exact/>
-     <Route path="/search/:keyword" component={Home}/>
-     <Route path="/login" component={Login} exact/>
-     <Route path="/product/:id" component={productDetails} exact/>
-     </div>
-     <Footer/>
-    </div>
+      <div className="App">
+        <Header />
+        <div className="container container-fluid home">
+          <Route path="/" component={Home} exact />
+          <Route path="/search/:keyword" component={Home} />
+          <Route path="/product/:id" component={productDetails} exact />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </div>
+        <Footer />
+      </div>
     </Router>
-  )
+  );
 }
 
 export default App;

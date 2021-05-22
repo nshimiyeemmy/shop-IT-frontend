fbf651d8b9040b0b63c6b8ee373c53b0e1b2e625
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-22 21:54:46 +0200

    added logout reducer

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -8,6 +8,8 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
   CLEAR_ERRORS,
 } from '../constants/userConstants';
 
@@ -28,13 +30,24 @@ export const authReducer = (state = { user: [] }, action) => {
         ...state,
         loading: false,
         isAuthenticated: true,
-        user: action.payload
+        user: action.payload,
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        loading: false,
+        isAuthenticated: false,
+        user: null,
       };
     case LOAD_USER_FAIL:
       return {
         loading: false,
         isAuthenticated: false,
         user: null,
+        error: action.payload,
+      };
+    case LOGOUT_FAIL:
+      return {
+        ...state,
         error: action.payload,
       };
     case LOGIN_FAIL:

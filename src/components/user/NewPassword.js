5ce693db37a8559018926c6ffcda92f42d740f07
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 16:45:35 +0200

    added new Password component

---
+++ src/components/user/NewPassword.js
@@ -0,0 +1,79 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { useAlert } from 'react-alert';
+import MetaData from '../layouts/MetaData';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetPassword, clearErrors } from '../../actions/userActions';
+
+const NewPassword = ({ history, match }) => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const alert = useAlert();
+  const dispatch = useDispatch();
+
+  const { error, success, loading } = useSelector(
+    (state) => state.resetPassword
+  );
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    if (success) {
+      alert.success('Password Reset Successfully');
+      history.push('/login');
+    }
+  }, [dispatch, alert, error, success, history]);
+
+  //creating the submitHandler to handle the email and password submit
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('password', password);
+    formData.append('confirmPassword', confirmPassword);
+    dispatch(resetPassword(match.params.token, formData));
+  };
+  return (
+    <Fragment>
+      <MetaData title={'New Password Reset'} />
+      <div className="row wrapper">
+        <div className="col-10 col-lg-5">
+          <form className="shadow-lg" onSubmit={submitHandler}>
+            <h1 className="mb-3">New Password</h1>
+
+            <div className="form-group">
+              <label htmlFor="password_field">Password</label>
+              <input
+                type="password"
+                id="password_field"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="confirm_password_field">Confirm Password</label>
+              <input
+                type="password"
+                id="confirm_password_field"
+                className="form-control"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              id="new_password_button"
+              type="submit"
+              className="btn btn-block py-3"
+            >
+              Set Password
+            </button>
+          </form>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default NewPassword;

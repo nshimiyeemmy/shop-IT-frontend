96f807bcecbb4a90f297a2316f3707f49856200f
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 16:44:49 +0200

    added newPassword actions and everything works fine as per this commit

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -18,6 +18,9 @@ import {
   FORGOT_PASSWORD_REQUEST,
   FORGOT_PASSWORD_SUCCESS,
   FORGOT_PASSWORD_FAIL,
+  NEW_PASSWORD_REQUEST,
+  NEW_PASSWORD_SUCCESS,
+  NEW_PASSWORD_FAIL,
   LOGOUT_SUCCESS,
   LOGOUT_FAIL,
   CLEAR_ERRORS,
@@ -57,7 +60,7 @@ export const register = (userData) => async (dispatch) => {
     const { data } = await axios.post(`/api/v1/register`, userData, config);
     dispatch({
       type: REGISTER_USER_SUCCESS,
-      payload: data.data.user,
+      payload: data.user,
     });
   } catch (error) {
     dispatch({
@@ -154,6 +157,32 @@ export const forgotPassword = (email) => async (dispatch) => {
   }
 };
 
+//Reset my password reducer
+export const resetPassword = (token, passwords) => async (dispatch) => {
+  try {
+    dispatch({ type: NEW_PASSWORD_REQUEST });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.put(
+      `/api/v1/password/reset/${token}`,
+      passwords,
+      config
+    );
+    dispatch({
+      type: NEW_PASSWORD_SUCCESS,
+      payload: data.success,
+    });
+  } catch (error) {
+    dispatch({
+      type: NEW_PASSWORD_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 //logout user action
 export const logoutUser = () => async (dispatch) => {
   try {
@@ -168,7 +197,6 @@ export const logoutUser = () => async (dispatch) => {
     });
   }
 };
-
 //Clear Erorrs using CLEAR_ERRORS constant
 export const clearErrors = () => async (dispatch) => {
   dispatch({

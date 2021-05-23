5b41e14dd58edc7489d21d118cce8842ce4d24e2
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 08:05:30 +0200

    updated with the forgot password

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -16,6 +16,9 @@ import {
   UPDATED_PASSWORD_SUCCESS,
   UPDATED_PASSWORD_RESET,
   UPDATED_PASSWORD_FAIL,
+  FORGOT_PASSWORD_REQUEST,
+  FORGOT_PASSWORD_SUCCESS,
+  FORGOT_PASSWORD_FAIL,
   LOGOUT_SUCCESS,
   LOGOUT_FAIL,
   CLEAR_ERRORS,
@@ -102,6 +105,39 @@ export const userReducer = (state = {}, action) => {
       };
     case UPDATED_PROFILE_FAIL:
     case UPDATED_PASSWORD_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//forgot password reducer
+export const forgotPasswordReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FORGOT_PASSWORD_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FORGOT_PASSWORD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        message: action.payload,
+      };
+
+    case FORGOT_PASSWORD_FAIL:
       return {
         ...state,
         loading: false,

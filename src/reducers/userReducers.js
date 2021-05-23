28c1f8ac5071be8aeab025315ca5e8015f61bf7e
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-23 16:46:34 +0200

    added newPassword reducer method and everything is working fine as per this commit

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -19,6 +19,9 @@ import {
   FORGOT_PASSWORD_REQUEST,
   FORGOT_PASSWORD_SUCCESS,
   FORGOT_PASSWORD_FAIL,
+  NEW_PASSWORD_REQUEST,
+  NEW_PASSWORD_SUCCESS,
+  NEW_PASSWORD_FAIL,
   LOGOUT_SUCCESS,
   LOGOUT_FAIL,
   CLEAR_ERRORS,
@@ -125,6 +128,7 @@ export const userReducer = (state = {}, action) => {
 export const forgotPasswordReducer = (state = {}, action) => {
   switch (action.type) {
     case FORGOT_PASSWORD_REQUEST:
+    case NEW_PASSWORD_REQUEST:
       return {
         ...state,
         loading: true,
@@ -136,8 +140,14 @@ export const forgotPasswordReducer = (state = {}, action) => {
         loading: false,
         message: action.payload,
       };
+    case NEW_PASSWORD_SUCCESS:
+      return {
+        ...state,
+        success: action.payload,
+      };
 
     case FORGOT_PASSWORD_FAIL:
+    case NEW_PASSWORD_FAIL:
       return {
         ...state,
         loading: false,

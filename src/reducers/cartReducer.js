35bcb779207e6e14d59965dd7aae4292af9f7e5f
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-26 07:48:33 +0200

    updated with remove product from cart reducer function

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,10 +1,12 @@
-import { ADD_TO_CART } from '../constants/cartConstants';
+import { ADD_TO_CART, REMOVE_ITEM_CART } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       const item = action.payload;
-      const isItemExists = state.cartItems.find((i) => i.product === item.product);
+      const isItemExists = state.cartItems.find(
+        (i) => i.product === item.product
+      );
       if (isItemExists) {
         return {
           ...state,
@@ -18,6 +20,11 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
           cartItems: [...state.cartItems, item],
         };
       }
+    case REMOVE_ITEM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter((i) => i.product !== action.payload),
+      };
     default:
       return state;
   }

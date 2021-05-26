90138ed75580df5dec51438fab03ad3cdcd46a3d
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-05-26 02:28:46 +0200

    added add_to_cart functionality to our app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/layouts/Header';
 import Footer from './components/layouts/Footer';
 import Home from './components/Home';
 import productDetails from './components/product/ProductDetails';
+
+import Cart from './components/cart/Cart';
+
 import Login from './components/user/Login';
 import Register from './components/user/Register';
 import Profile from './components/user/Profile';
@@ -27,6 +30,9 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home} />
           <Route path="/product/:id" component={productDetails} exact />
+
+          <Route path="/cart" component={Cart} exact />
+
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <ProtectedRoute path="/me" component={Profile} exact />
@@ -41,5 +47,4 @@ function App() {
     </Router>
   );
 }
-
 export default App;

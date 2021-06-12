d91db5860b384262cd01c2d8e7a842a6802ebc98
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 19:13:27 +0200

    updated

--- src/components/cart/OrderSuccess.js
+++ src/components/cart/OrderSuccess.js
@@ -8,14 +8,13 @@ const OrderSuccess = () => {
       <div className="row justify-content-center">
         <div className="col-6 mt-5 text-center">
           <img
-            className="my-4 img-fluid d-block mx-auto success"
+            className="my-5 img-fluid d-block mx-auto success"
             src="https://freepngimg.com/thumb/success/6-2-success-png-image.png"
             alt="Order Success"
             width="80"
             height="100"
           />
           <h2>Your Order has been placed successfully.</h2>
-
           <Link to="/orders/me">Go to Orders</Link>
         </div>
       </div>

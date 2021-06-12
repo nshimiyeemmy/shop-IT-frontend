7857888c321212f051a1429ae35f2dfd1ddddd43
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 18:35:38 +0200

    added the orderSuccess component

---
+++ src/components/cart/OrderSuccess.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import MetaData from '../layouts/MetaData';
+const OrderSuccess = () => {
+  return (
+    <Fragment>
+      <MetaData title={'Order Success'} />
+      <div className="row justify-content-center">
+        <div className="col-6 mt-5 text-center">
+          <img
+            className="my-4 img-fluid d-block mx-auto success"
+            src="https://freepngimg.com/thumb/success/6-2-success-png-image.png"
+            alt="Order Success"
+            width="80"
+            height="100"
+          />
+          <h2>Your Order has been placed successfully.</h2>
+
+          <Link to="/orders/me">Go to Orders</Link>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+export default OrderSuccess;

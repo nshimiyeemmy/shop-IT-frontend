039226f00b8014edd87dcdf6f916310bfaf4603d
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 19:14:03 +0200

    updated with the get all user logged in orders action function

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -4,6 +4,9 @@ import {
   CREATE_ORDER_REQUEST,
   CREATE_ORDER_SUCCESS,
   CREATE_ORDER_FAIL,
+  MY_ORDERS_REQUEST,
+  MY_ORDERS_SUCCESS,
+  MY_ORDERS_FAIL,
   CLEAR_ERRORS,
 } from '../constants/orderConstants';
 
@@ -27,6 +30,24 @@ export const createOrder = (order) => async (dispatch, getState) => {
     });
   }
 };
+
+//getting all loggedIn user Orders history
+export const myOrders = () => async (dispatch) => {
+  try {
+    dispatch({ type: MY_ORDERS_REQUEST });
+    const { data } = await axios.get('/api/v1/orders/me');
+    dispatch({
+      type: MY_ORDERS_SUCCESS,
+      payload: data.Orders,
+    });
+  } catch (error) {
+    dispatch({
+      type: MY_ORDERS_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
 //Clear Erorrs using CLEAR_ERRORS constant
 export const clearErrors = () => async (dispatch) => {
   dispatch({

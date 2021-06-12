53c22845d55b500910fdb9fac53782617065d1b8
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-12 15:51:38 +0200

    updated with a submitHandler function to handle the payment process

--- src/components/cart/Payment.js
+++ src/components/cart/Payment.js
@@ -36,6 +36,57 @@ const Payment = ({ history }) => {
   const { cartItems, shippingInfo } = useSelector((state) => state.cart);
 
   useEffect(() => {}, []);
+
+  //getting orderInfo from the session storage
+  const orderInfo = JSON.parse(sessionStorage.getItem('OrderInfo'));
+  const paymentData = {
+    amount: Math.round(orderInfo.totalPrice * 100),
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    document.querySelector('#pay_btn').disabled = true;
+    let res;
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      res = await axios.post('/api/v1/payment/process', paymentData, config);
+      const clientSecret = res.data.client_secret;
+      if (!stripe || !elements) {
+        return;
+      }
+      const result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardNumberElement),
+          billing_details: {
+            name: user.firstname,
+            email: user.email,
+          },
+        },
+      });
+      console.log("reached here");
+      if (result.error) {
+        alert.error(result.error.message);
+        document.querySelector('#pay_btn').disabled = false;
+      }
+      else {
+        //Check whether the payment is processed or not
+        if (result.paymentIntent.status === 'succeeded') {
+        //   TODO : Place New Order after the payment is recieved
+          history.push('/success');
+        } else {
+          alert.error(
+            'There was some Issue while processing your Payment'
+          );
+        }
+      }
+    } catch (error) {
+      document.querySelector('#pay_btn').disabled = false;
+      alert.error(error.response.data.message);
+    }
+  };
   return (
     <Fragment>
       <MetaData title={'Payment Process'} />
@@ -43,7 +94,7 @@ const Payment = ({ history }) => {
 
       <div className="row wrapper">
         <div className="col-10 col-lg-5">
-          <form className="shadow-lg">
+          <form className="shadow-lg" onSubmit={submitHandler}>
             <h1 className="mb-4">Card Info</h1>
             <div className="form-group">
               <label htmlFor="card_num_field">Card Number</label>
@@ -76,7 +127,7 @@ const Payment = ({ history }) => {
             </div>
 
             <button id="pay_btn" type="submit" className="btn btn-block py-3">
-              Pay
+              Pay {`- ${orderInfo && orderInfo.totalPrice}`}
             </button>
           </form>
         </div>

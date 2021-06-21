bfd3e78febdafffec32dbe85d071c2c83ce07b84
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-21 12:03:16 +0200

    updated

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -21,7 +21,7 @@ const Product = ({product,col}) => {
                 <div className="rating-inner" style={{width:`${(product.ratings / 5) * 100}%`}}>
                 </div>
               </div>
-              <span id="no_.of_reviews">({product.numOfReviews} Reviews)</span>
+              <span id="no_of_reviews">{product.numOfReviews} Reviews</span>
             </div>
             <p className="card-text">${product.price}</p>
             <Link to={`/product/${product._id}`} id="view_btn" className="btn btn-block">View Details</Link>

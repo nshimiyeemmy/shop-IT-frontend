a4fa68349142cb7c8ee803a2d2a31d6cb537de4e
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-21 09:47:17 +0200

    updated

--- src/reducers/orderReducers.js
+++ src/reducers/orderReducers.js
@@ -42,6 +42,7 @@ export const myOrdersReducer = (state = { orders: [] }, action) => {
   switch (action.type) {
     case MY_ORDERS_REQUEST:
       return {
+        ...state,
         loading: true,
       };
     case MY_ORDERS_SUCCESS:
@@ -50,6 +51,34 @@ export const myOrdersReducer = (state = { orders: [] }, action) => {
         orders: action.payload,
       };
     case MY_ORDERS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+//GET ORDER DETAILS REDUCER
+export const OrderDetailsReducer = (state = { order: {} }, action) => {
+  switch (action.type) {
+    case ORDER_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case ORDER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        order: action.payload,
+      };
+    case ORDER_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,

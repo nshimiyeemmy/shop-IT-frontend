49e18a4748edb0333b552b4ba76165392847cd87
Nshimiye Emmy <nshimiyee311@gmail.com>
2021-06-21 09:45:57 +0200

    updated with reducer to add the product review

--- src/reducers/productReducers.js
+++ src/reducers/productReducers.js
@@ -1,72 +1,103 @@
 import {
-    ALL_PRODUCTS_REQUEST,
-    ALL_PRODUCTS_SUCCESS,
-    ALL_PRODUCTS_FAIL,
+  ALL_PRODUCTS_REQUEST,
+  ALL_PRODUCTS_SUCCESS,
+  ALL_PRODUCTS_FAIL,
+  PRODUCTS_DETAILS_REQUEST,
+  PRODUCTS_DETAILS_SUCCESS,
+  PRODUCTS_DETAILS_FAIL,
+  NEW_REVIEW_REQUEST,
+  NEW_REVIEW_RESET,
+  NEW_REVIEW_SUCCESS,
+  NEW_REVIEW_FAIL,
+  CLEAR_ERRORS,
+} from '../constants/productConstants';
 
-    PRODUCTS_DETAILS_REQUEST,
-    PRODUCTS_DETAILS_SUCCESS,
-    PRODUCTS_DETAILS_FAIL,
+export const productsReducers = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case ALL_PRODUCTS_REQUEST:
+      return {
+        loading: true,
+        products: [],
+      };
+    case ALL_PRODUCTS_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload.products,
+        productsCount: action.payload.productsCount,
+        resPerPage: action.payload.resPerPage,
+        FilteredProductsCount: action.payload.FilteredProductsCount,
+      };
+    case ALL_PRODUCTS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
 
-    CLEAR_ERRORS
- }  from '../constants/productConstants'
+export const productDetailsReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCTS_DETAILS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case PRODUCTS_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        product: action.payload.product,
+      };
+    case PRODUCTS_DETAILS_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
 
-export const productsReducers = (state = {products:[]}, action) =>{
-    switch(action.type){
-      case ALL_PRODUCTS_REQUEST:
-          return{
-              loading:true,
-              products:[]
-          }
-          case ALL_PRODUCTS_SUCCESS:
-            return{
-                loading:false,
-                products:action.payload.products,
-                productsCount:action.payload.productsCount,
-                resPerPage:action.payload.resPerPage,
-                FilteredProductsCount:action.payload.FilteredProductsCount
-
-            }
-            case ALL_PRODUCTS_FAIL:
-            return{
-                loading:false,
-                error:action.payload
-            }
-            case CLEAR_ERRORS:
-                    return{
-                        ...state,
-                        error:null
-                    }
-                default:
-                return state;
-    }
-}
-
-export const productDetailsReducer  = (state = {product:{}}, action) =>{
-
-    switch(action.type){
-        case PRODUCTS_DETAILS_REQUEST:
-            return{
-                ...state,
-                loading:true,
-
-            }
-            case PRODUCTS_DETAILS_SUCCESS:
-              return{
-                  loading:false,
-                  product:action.payload.product
-                }
-              case PRODUCTS_DETAILS_FAIL:
-              return{
-                  ...state,
-                  error:null
-              }
-              case CLEAR_ERRORS:
-                      return{
-                          ...state,
-                          error:null
-                      }
-                  default:
-                  return state;
-      }
-
-}
+// PRODUCT REVIEW
+export const newReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case NEW_REVIEW_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case NEW_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: action.payload,
+      };
+    case NEW_REVIEW_FAIL:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case NEW_REVIEW_RESET:
+      return {
+        ...state,
+        success:false
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
